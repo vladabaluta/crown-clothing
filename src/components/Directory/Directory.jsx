0513fec08ec9de4f45d883e8c8a7bd2f6ac02675
vladabaluta2806 <vladabaluta2806@gmail.com>
2020-09-25 16:14:33 +0300

    replace something

--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -8,36 +8,36 @@ class Directory extends Component {
     this.state = {
       sections: [
         {
-          title: "hats",
+          title: "Hats",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
           id: 1,
           linkUrl: "shop/hats",
         },
         {
-          title: "jackets",
+          title: "Jackets",
           imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
           id: 2,
           linkUrl: "shop/jackets",
         },
         {
-          title: "sneakers",
+          title: "Sneakers",
           imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
           id: 3,
           linkUrl: "shop/sneakers",
         },
         {
-          title: "womens",
+          title: "Women",
           imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
           size: "large",
           id: 4,
-          linkUrl: "shop/womens",
+          linkUrl: "shop/Womens",
         },
         {
-          title: "mens",
+          title: "Men",
           imageUrl: "https://i.ibb.co/R70vBrQ/mens.png",
           size: "large",
           id: 5,
-          linkUrl: "shop/mens",
+          linkUrl: "shop/Mens",
         },
       ],
     };
